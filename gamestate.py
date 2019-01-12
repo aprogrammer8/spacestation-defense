8e06f656cb3e2929465cc3bdf39c774a9ebab839
Ryan Westlund <rlwestlund@gmail.com>
2019-01-12 17:04:11 -0500

    Incremental update on gamestate classes

--- gamestate.py
+++ gamestate.py
@@ -1,10 +1,72 @@
 class Gamestate:
-	def __init__(self, filename='', mission=''):
-		if filename:
-			self.load(filename)
+	def __init__(self, json='', mission='mission_basic'):
+		if jsom:
+			self.load_json(json)
 			return
-		self.enemy_ships = []
-		self.allied_ships = []
-		self.station = Station()
-		self.wave = 0
-		self.mission = # Some kind of read_mission fumc? I need a mission class too
+		else: # If no JSON, we load the starting state of the mission.
+			self.mission = mission
+			self.load_mission(mission)
+			#self.enemy_ships = []
+			#self.allied_ships = []
+			self.station = Station()
+			self.wave = 0
+			self.round = 0
+
+# An Entity is anything with a position on the board.
+class Entity:
+	def __init__(self, window, pos, image, rot, hull, shield=0, shield_regen=(0,), weapons=()):
+		self.window = window # Set to none on the server side.
+		# Position on the grid. Window pixel position is calculated from this.
+		self.pos = pos
+		self.image = image
+		# Rotation is stored as as 2-element tuple, like (0, -1) for up.
+		self.rot = rot
+		self.maxhull = self.hull = hull
+		self.maxshield = self.shield = shield
+		self.shield_regen_amounts = shield_regen_amounts
+		self.shield_regen_pointer = 0
+		self.already_moved = False
+		self.stored_action = None
+	def move(self, change):
+		self.pos[0] += change[0]
+		self.pos[1] += change[1]
+		if self.window:pass # TODO: animation of moving
+
+
+class Ship(Entity):
+	def __init__(self, window, pos, image, rot, hull, shield, shield_regen, speed, salvage, wave=0, size=0):
+		Entity.__init__(self, window=window, pos=pos, image=image, rot=rot, hull=hull, shield=shield, shield_regen=shield_regen)
+		self.speed = speed
+		# Enemy only fields.
+		self.wave = wave
+		# Ally only fields.
+		self.size = size
+		# This field is used for both, but means something different.
+		self.salvage = salvage
+
+
+# A Station Component.
+class Component(Entity):
+	def __init__(self, window, station, pos, image, rot, hull):
+		Entity.__init__(self, window=window, pos=pos, image=image, rot=rot, hull=hull, shield=0, shield_regen=(0,))
+	@property
+	def shield(self):
+		return 100 # calculate which shield generators are applying to it
+
+class Weapon():
+	def __init__(self, type, power):
+		self.type = type
+		self.power = power
+
+class Mission:
+	def __init__(self):
+		pass
+		# There should be a shitton of tunable parameters in here.
+	def query(self):
+		# This method will return a list of enemies, a reward for clearing it, a number of turns till the next wave.
+		return None
+
+
+# The test mission.
+mission_basic = Mission()
+
