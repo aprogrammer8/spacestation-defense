7a27954eb0431e01119971760b1d83244b4c7b5c
Ryan Westlund <rlwestlund@gmail.com>
2019-01-20 17:24:56 -0500

    Make server.py proceed when all players are done

--- server.py
+++ server.py
@@ -2,12 +2,19 @@ import socket, selectors, random, sys, json
 from gamestate import *
 from sockets import *
 
+class Player:
+	def __init__(self, name):
+		self.name = name
+		# self.done is used during the collect_input phase.
+		self.done = False
+
+
 def collect_input():
 	global sock, players, gamestate
 	while True:
 		msg = recv_message(sock)
 		print(msg)
-		player = msg[:msg.index(':')]
+		player_name = msg[:msg.index(':')]
 		msg = msg[msg.index(':')+1:]
 		# Assigning an action to a ship or component.
 		if msg.startswith("ASSIGN:"):
@@ -20,6 +27,18 @@ def collect_input():
 		# Playing a card.
 		elif msg.startswith("PLAY:"):
 			pass
+		# When a player is ready to proceed.
+		elif msg == "DONE":
+			for player in players:
+				if player.name == player_name:
+					player.done = True
+					break
+			done = True
+			for player in players:
+				if not player.done:
+					done = False
+					break
+			if done: break
 
 
 def main():
@@ -30,7 +49,9 @@ def main():
 	#selector.register(sock, selectors.EVENT_READ)
 	sock.connect(sys.argv[1])
 	# Receive the list of players
-	players = recv_message(sock).split(",")
+	player_names = recv_message(sock).split(",")
+	players = []
+	for name in player_names: players.append(Player(name))
 	mission = recv_message(sock)
 	gamestate = Gamestate(players, mission)
 	changes = gamestate.draw_cards(gamestate.mission.starting_cards)
@@ -42,6 +63,7 @@ def main():
 		changes = gamestate.upkeep()
 		if changes: sock.send(encode("SPAWN ENEMIES:" + json.dumps(changes)))
 		collect_input()
+		sock.send(encode("PLAYER INPUT COLLECTED"))
 		players_move()
 		enemies_move()
 		# Dump gamestate to a file so it can be restored in case of a crash.
